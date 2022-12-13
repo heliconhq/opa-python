67f617b158a9f33c64ef168ba1a5abbb724d3da9
Gustaf Sjoberg <gs@trell.se>
2022-12-13 11:58:53 +0100

    Add sphinx rtd theme.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,25 +1,22 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
+import os
+import sys
+import sphinx_rtd_theme
 
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+sys.path.insert(0, os.path.abspath('../..'))
+
+autoclass_content = 'both'
 
 project = 'opa-python'
 copyright = '2022, Gustaf Sjoberg'
 author = 'Gustaf Sjoberg'
 
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
-extensions = ['sphinx.ext.autodoc']
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx_rtd_theme',
+]
 
 templates_path = ['_templates']
 exclude_patterns = []
 
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
-html_theme = 'alabaster'
+html_theme = "sphinx_rtd_theme"
 html_static_path = ['_static']
